400a4c9ce58a8139bf9873697951ee1fde37754b
oakes <zsoakes@gmail.com>
2016-05-25 11:49:59 -0400

    Add test for save/load methods

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -16,7 +16,7 @@ public class Main {
     static Player player;
 
     public static void main(String[] args) throws Exception {
-        player = loadGame();
+        player = loadGame(SAVE_FILE);
         if (player == null) {
             player = new Player();
             System.out.println("Starting new game.");
@@ -47,7 +47,7 @@ public class Main {
         System.out.println(player);
         System.out.println(ogre);
 
-        saveGame();
+        saveGame(player, SAVE_FILE);
     }
 
     public static String nextLine() {
@@ -59,7 +59,7 @@ public class Main {
                 }
             }
             else if (line.equals("/save")) {
-                saveGame();
+                saveGame(player, SAVE_FILE);
             }
             else {
                 System.out.println("Command not found.");
@@ -70,11 +70,11 @@ public class Main {
         return line;
     }
 
-    public static void saveGame() {
+    public static void saveGame(Player player, String filename) {
         JsonSerializer serializer = new JsonSerializer();
         String json = serializer.include("*").serialize(player);
 
-        File f = new File(SAVE_FILE);
+        File f = new File(filename);
         try {
             FileWriter fw = new FileWriter(f);
             fw.write(json);
@@ -84,8 +84,8 @@ public class Main {
         }
     }
 
-    public static Player loadGame() {
-        File f = new File(SAVE_FILE);
+    public static Player loadGame(String filename) {
+        File f = new File(filename);
         try {
             Scanner scanner = new Scanner(f);
             scanner.useDelimiter("\\Z");
